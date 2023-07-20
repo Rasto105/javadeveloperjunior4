9d9feebefae7cf6cca9c0c4d5439e50ff41792c7
Rasto105 <r.zulko@gmail.com>
2023-07-20 15:15:51 +0200

    cvc

---
+++ src/main/java/enums/OblubenaFarbaMain.java
@@ -0,0 +1,7 @@
+package enums;
+
+public class OblubenaFarbaMain {
+    public static void main(String[] args) {
+        System.out.println("moja oblubena je: "+ Farba.MODRÁ.toString().toLowerCase());
+    }
+}
